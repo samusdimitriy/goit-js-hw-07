7a49ea392a84e72c9522752dee206c033a13767b
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-03-24 07:55:52 +0100

    corrected the code and removed unnecessary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,66 +1,40 @@
 import { galleryItems } from "./gallery-items.js";
 
-const supportsLazyLoading = "loading" in HTMLImageElement.prototype;
-
-if (!supportsLazyLoading) {
-  const script = document.createElement("script");
-  script.src = "https://cdn.jsdelivr.net/npm/lazysizes@5.3.2/lazysizes.min.js";
-  const lazysizesPlaceholder = document.getElementById("lazysizes-placeholder");
-  lazysizesPlaceholder.parentNode.insertBefore(script, lazysizesPlaceholder);
-}
-
 const palette = document.querySelector(".gallery");
-const cardsMarkup = createCardsMarkup(galleryItems);
-palette.insertAdjacentHTML("beforeend", cardsMarkup);
+palette.insertAdjacentHTML("beforeend", createCardsMarkup(galleryItems));
 palette.addEventListener("click", onGalleryClick);
 
-function createCardsMarkup(galleryItems) {
-  // Избегайте отложенной загрузки изображений, которые находятся в первой видимой области просмотра - написано в документации, поэтому я буду использовать lazysizes только в том случае, если пользователь открыл сайт на мобильном.
-  const isMobileScreen = window.innerWidth <= 768;
-
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      const imgClass =
-        isMobileScreen && !supportsLazyLoading
-          ? "gallery__image lazyload"
-          : "gallery__image";
-
-      const imgSrc = isMobileScreen
-        ? supportsLazyLoading
-          ? `loading="lazy" src="${preview}"`
-          : `data-src="${preview}"`
-        : `src="${preview}"`;
-
-      return `<li class="gallery__item">
-        <a class="gallery__link" href="${original}">
-        <img class="${imgClass}" ${imgSrc} data-source="${original}" alt="${description}"></img>
-        </a>
-        </li>`;
-    })
+function createCardsMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+      </a>
+    </li>`
+    )
     .join("");
 }
 
 function onGalleryClick(event) {
   event.preventDefault();
-  if (!event.target.classList.contains("gallery__image")) {
-    return;
-  }
+  const { target } = event;
+  if (target.nodeName !== "IMG") return;
 
-  document.body.style.overflow = "hidden";
-  document.body.style.height = "100vh";
-
-  const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-    `);
+  const instance = basicLightbox.create(
+    `<img src="${target.dataset.source}" width="800" height="600">`
+  );
   instance.show();
+  document.body.style = "overflow: hidden; height: 100vh;";
 
-  window.addEventListener("keydown", onEscKeyClick, { once: true });
+  window.addEventListener("keydown", onEscKeyClick);
 
   function onEscKeyClick(event) {
-    if (event.code === "Escape" && instance && instance.visible()) {
+    if (event.code === "Escape" && instance.visible()) {
       instance.close();
-      document.body.style.overflow = "";
-      document.body.style.height = "";
+      document.body.style = "";
+      window.removeEventListener("keydown", onEscKeyClick);
     }
   }
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,5 @@
 import { galleryItems } from "./gallery-items.js";
 
-const supportsLazyLoading = "loading" in HTMLImageElement.prototype;
-loadLazySizesIfNeeded();
-
 const palette = document.querySelector(".gallery");
 const cardsMarkup = createCardsMarkup(galleryItems);
 palette.insertAdjacentHTML("beforeend", cardsMarkup);
@@ -11,36 +8,12 @@ palette.addEventListener("click", (event) =>
   onGalleryClick(event, galleryItems)
 );
 
-function loadLazySizesIfNeeded() {
-  if (!supportsLazyLoading) {
-    const script = document.createElement("script");
-    script.src =
-      "https://cdn.jsdelivr.net/npm/lazysizes@5.3.2/lazysizes.min.js";
-    const lazysizesPlaceholder = document.getElementById(
-      "lazysizes-placeholder"
-    );
-    lazysizesPlaceholder.parentNode.insertBefore(script, lazysizesPlaceholder);
-  }
-}
-
 function createCardsMarkup(items) {
-  const isMobileScreen = window.innerWidth <= 768;
-
   return items
     .map(({ preview, original, description }) => {
-      const imgClass =
-        isMobileScreen && !supportsLazyLoading
-          ? "gallery__image lazyload"
-          : "gallery__image";
-      const imgSrc = isMobileScreen
-        ? supportsLazyLoading
-          ? `loading="lazy" src="${preview}"`
-          : `data-src="${preview}"`
-        : `src="${preview}"`;
-
       return `<li class="gallery__item">
         <a class="gallery__link" href="${original}" data-caption="${description}">
-        <img class="${imgClass}" src="${preview}" data-source="${original}" alt="${description}"></img>
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
         </a>
         </li>`;
     })
@@ -49,30 +22,22 @@ function createCardsMarkup(items) {
 
 function onGalleryClick(event, items) {
   event.preventDefault();
-  if (!event.target.classList.contains("gallery__image")) {
+  if (event.target.nodeName !== "IMG") {
     return;
   }
 
   const gallery = new SimpleLightbox(".gallery a", {
     captionSelector: "self",
     captionType: "attr",
-    captionsData: "title",
     captionsData: "data-caption",
     captionDelay: 250,
+    closeOnEscape: true,
   });
 
   const clickedImageIndex = items.findIndex(
     (item) => item.original === event.target.dataset.source
   );
   gallery.open(clickedImageIndex);
-
-  window.addEventListener("keydown", onEscKeyClick, { once: true });
-
-  function onEscKeyClick(event) {
-    if (event.code === "Escape" && gallery && gallery.isOpen()) {
-      gallery.close();
-    }
-  }
 }
 
 console.log(galleryItems);
